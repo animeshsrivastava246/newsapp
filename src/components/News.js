14d7a34cc953ac0ec07848a93d5fd0d2e48d36c2
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2021-08-31 20:26:58 +0530

    Finally next button fixed totalResult -> totalResults

--- src/components/News.js
+++ src/components/News.js
@@ -7,12 +7,12 @@ export class News extends Component {
     static defaultProps= {
         pgSize: 8,
         country: 'in',
-        category: 'general'
+        category: 'general',
     }
     static propTypes = {
         pgSize: PropTypes.number,
         country: PropTypes.string,
-        category: PropTypes.string
+        category: PropTypes.string,
     }
     constructor(){
         super();
@@ -23,38 +23,28 @@ export class News extends Component {
         }
     }
 
-    async componentDidMount(){
+    async updateNews(){
         this.setState({loading: true})
-        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c5849d23c67441a8bcbc80e68c20d1b7&page=1&pageSize=${this.props.pgSize}`);
+        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c5849d23c67441a8bcbc80e68c20d1b7&page=${this.state.page}&pageSize=${this.props.pgSize}`);
         let parsedData = await data.json();
         this.setState({
             articles: parsedData.articles ,
-            totalResult: parsedData.totalResult ,
+            totRes: parsedData.totalResults ,
             loading: false
         })
+    }
+
+    async componentDidMount(){
+        this.updateNews();
     }
     
     handlePrevClick = async ()=>{
-        this.setState({loading: true})
-        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c5849d23c67441a8bcbc80e68c20d1b7&page=${this.state.page - 1}&pageSize=${this.props.pgSize}`);
-        let parsedData = await data.json();
-        this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading: false
-        })
+        this.setState({page: this.state.page-1});
+        this.updateNews();
     }
     handleNextClick = async ()=>{
-        if(!(this.state.page + 1 > Math.ceil(this.state.totalResult/this.props.pgSize))){
-            this.setState({loading: true})
-            let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c5849d23c67441a8bcbc80e68c20d1b7&page=${this.state.page + 1}&pageSize=${this.props.pgSize}`);
-            let parsedData = await data.json();
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles,
-                loading: false
-            })
-        }
+        this.setState({page: this.state.page+1});
+        this.updateNews();
     }
 
     render() {
@@ -72,7 +62,7 @@ export class News extends Component {
                 </div>
                 <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous Page</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResult/this.props.pgSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next Page &rarr;</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totRes / this.props.pgSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next Page &rarr;</button>
                 </div>
             </div>
         )
